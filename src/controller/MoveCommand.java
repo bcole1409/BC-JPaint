27cdca78617d9c496c3396a770c2cbf98762b91f
Brandon Cole <bcole5@depaul.edu>
2021-03-06 23:09:08 -0600

    WIP: Move Undo (Groups)

--- src/controller/MoveCommand.java
+++ src/controller/MoveCommand.java
@@ -90,19 +90,23 @@ public class MoveCommand implements IUndoable, ICommand {
 
     @Override
     public void undo() {
+
+        //---UNDO CODE BLOCK FOR SELECTEDSHAPESLIST AND DRAWLIST---
         //1. we need to calc deltax, deltay
+
         //System.out.println("inside undo move command. My DrawList size is: " + drawList.size());
         //System.out.println("inside undo move command: " + masterJPaintController.drawList.toString());
-        int deltax = BoundsUtility.calcDeltaX(pressedPoint,releasedPoint);
-        int deltay = BoundsUtility.calcDeltaY(pressedPoint,releasedPoint);
+        int deltaX = BoundsUtility.calcDeltaX(pressedPoint,releasedPoint);
+        int deltaY = BoundsUtility.calcDeltaY(pressedPoint,releasedPoint);
+
         //clear selectedShapesList
         JPaintController.selectedShapesList = new ArrayList<ShapeCommand>();
 
-        //3. remove moved shapes from the drawlist
+
         for(ShapeCommand mySelectedShape : originalShapes) {
-            //System.out.println("MySelectedShape: " + mySelectedShape.p1.x + ", " + mySelectedShape.p1.y + "      " + mySelectedShape.p2.x + ", " + mySelectedShape.p2.y);
-            ShapeCommand shapeInMovedPosition = MoveUtility.CreateShapeGivenMovement(masterJPaintController.drawList, paintCanvas, mySelectedShape, deltax, deltay);
-            //System.out.println("A DrawList size: " + masterJPaintController.drawList.size());
+            //2. remove moved shapes from the drawlist
+            ShapeCommand shapeInMovedPosition = MoveUtility.CreateShapeGivenMovement(masterJPaintController.drawList, paintCanvas, mySelectedShape, deltaX, deltaY);
+
             ArrayList<IShape> tempDrawList = new ArrayList<IShape>();
 
             for (IShape myShape : masterJPaintController.drawList) {
@@ -112,11 +116,34 @@ public class MoveCommand implements IUndoable, ICommand {
             }
 
             masterJPaintController.drawList = (ArrayList<IShape>) tempDrawList.clone();
-            //System.out.println("B DrawList size: " + masterJPaintController.drawList.size());
+            //3. add original shape back into drawList
             masterJPaintController.drawList.add(mySelectedShape);
+
             //selected shapes list is never updated
             JPaintController.selectedShapesList.add(mySelectedShape);
+
             //System.out.println("C DrawList size: " + masterJPaintController.drawList.size());
         }
+
+        //---UNDO CODE BLOCK FOR GROUPS---
+        ArrayList<ShapeCommand> movedShapesList = new ArrayList<ShapeCommand>();
+        for(ShapeCommand myShape : originalShapes){
+            ShapeCommand movedShape = MoveUtility.CreateShapeGivenMovement(masterJPaintController.drawList, paintCanvas, myShape, deltaX, deltaY);
+            movedShapesList.add(movedShape);
+        }
+
+        ArrayList<GroupCommand> newJPCListOfGroups = new ArrayList<GroupCommand>();
+        for(GroupCommand myGroup : masterJPaintController.listOfGroups){
+            if(myGroup.containsAtLeastOneShape(movedShapesList)){
+                GroupCommand newMovedGroup = MoveUtility.CreateGroupGivenMovement(masterJPaintController.drawList, paintCanvas, myGroup, deltaX, deltaY);
+                newMovedGroup.drawMe(); //Only reason is to calculate TopLeft/BottomRight Corners. Don't care about actual drawing.
+                newJPCListOfGroups.add(newMovedGroup);
+            }
+            else{
+                newJPCListOfGroups.add(myGroup);
+            }
+        }
+        masterJPaintController.listOfGroups = newJPCListOfGroups;
+        System.out.println("148 size: " + masterJPaintController.listOfGroups.size());
     }
 }
